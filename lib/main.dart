83f9c2d19e5569775c8d09899edcd5d77b9c5e5c
Mahmoud Kamal <213179+mahmoudkamalx17@users.noreply.github.com>
2024-08-22 02:52:19 +0300

    Home Screen - added assets - added splash screen - added routes (routes - app router) - added icon app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stopwatch/core/routes/app_router.dart';
 import 'package:stopwatch/stopwatch_app.dart';
 
-void main() {
-  runApp(const StopwatchApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  runApp(StopwatchApp(appRouter: AppRouter()));
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:stopwatch/core/routes/app_router.dart';
 
-import 'package:stopwatch/main.dart';
+import 'package:stopwatch/stopwatch_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(StopwatchApp(appRouter: AppRouter()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
